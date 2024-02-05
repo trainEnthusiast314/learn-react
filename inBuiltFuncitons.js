34f52edab85f1519208204ffa4e71974b638d371
trainEnthusiast314 <trainsAreCool@protonmail.com>
2024-02-05 11:31:34 +0000

    list

--- inBuiltFuncitons.js
+++ inBuiltFuncitons.js
@@ -13,3 +13,12 @@ newArr.forEach((item) => {
 
 console.log(newArr);
 console.log(fourArr);
+
+const sum = fourArr.reduce((acc, curr) => {
+  return acc + curr;
+});
+
+let a = { x: 10, y: 20, z: 40 };
+let b = { a: "tiger", b: "lion", c: "sealion" };
+let c = { ...a, ...b, m: "acrobat", n: 26 };
+console.log(c);
--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const list = [
+    {
+      id: 1,
+      body: "First Item",
+      severity: 1,
+      status: "bad",
+    },
+    {
+      id: 2,
+      body: "Second Item",
+      severity: 2,
+      status: "good",
+    },
+    {
+      id: 3,
+      body: "Third Item",
+      severity: 5,
+      status: "Catastrophic",
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {list.map((item) => {
+        return (
+          <ul key={item.id}>
+            <li>{item.body}</li>
+            <li>{item.severity}</li>
+            <li>{item.status}</li>
+          </ul>
+        );
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
